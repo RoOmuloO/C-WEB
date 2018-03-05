a4cf94e2fb4fdf6524f541fea2c459df2f733bbc
RoOmuloO <romuloroger.contatos@gmail.com>
2018-03-04 21:19:38 -0300

    Trabalhando com NHibernate

---
+++ AulaModelo.Modelo/DB/DbFactory.cs
@@ -0,0 +1,89 @@
+﻿using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaModelo.Modelo.DB
+{
+    public class DbFactory
+    {
+        //Singletton serve para garantir que a instância sempre estará ativa.
+
+        private static DbFactory _instance = null;
+
+        private DbFactory()
+        {
+            Conexao();
+        }
+
+        public static DbFactory Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new DbFactory();
+                }
+
+                return _instance;
+            }
+        }
+
+        //Tratamento de exceção...
+        private void Conexao()
+        {
+            try
+            {
+                var stringConexao = "";
+
+
+                ConfigurarNHibernate(stringConexao);
+            }catch(Exception ex)
+            {
+                throw new Exception("Não foi possível conectar", ex);
+            }
+        }
+
+        private void ConfigurarNHibernate(String stringConexao)
+        {
+            try
+            {
+
+                var config = new Configuration();
+
+                //Configuração do NH com o MySQL
+                config.DataBaseIntegration(i =>
+                {
+                    // Dialeto do Banco
+                    i.Dialect<NHibernate.Dialect.MsSql7Dialect>();
+
+                    // Conexão string
+                    i.ConnectionString = stringConexao;
+
+                    //Drive de conexão com o banco
+                    i.Driver<NHibernate.Driver.MySqlDataDriver>();
+
+                    //Provedor de conexão do MySQL
+                    i.ConnectionProvider<NHibernate.Connection.DriverConnectionProvider>();
+
+                    // Gera Log dos SQL Executados no console
+                    i.LogSqlInConsole = true;
+
+                    //Descomentar caso queira visualizar o log de SQL Formatado no console
+                    i.LogFormattedSql = true;
+
+                    // Cria o Schema do banco de dados sempre que a Configuration for utilizar
+                    i.SchemaAction = SchemaAutoAction.Update;
+
+                });
+
+
+            }catch(Exception ex)
+            {
+                throw new Exception("Não foi possivel configurar NH", ex);
+            }
+        }
+    }
+}
--- AulaModelo.Modelo/DB/Model/Pessoa.cs
+++ AulaModelo.Modelo/DB/Model/Pessoa.cs
@@ -1,3 +1,5 @@
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +12,34 @@ namespace AulaModelo.Modelo.DB.Model
     {
         public static List<Pessoa> Pessoas = new List<Pessoa>();
 
-        public Guid Id { get; set; }
-        public String Nome { get; set; }
-        public DateTime DtNascimento { get; set; }
-        public String Email { get; set; }
-        public String Telefone { get; set; }
+        public virtual Guid Id { get; set; }
+        public virtual String Nome { get; set; }
+        public virtual DateTime DtNascimento { get; set; }
+        public virtual String Email { get; set; }
+        public virtual String Telefone { get; set; }
+    }
+
+    public class PessoaMap : ClassMapping<Pessoa>
+    {
+        public PessoaMap()
+        {
 
 
+            Id(x => x.Id, m => m.Generator(Generators.Guid));
+
+            Property(x => x.Nome);
+            Property(x => x.DtNascimento);
+            Property(x => x.Telefone);
+            Property(x => x.Email);
+
+            ////Importando de uma base ja existente..
+            //Table("tblPessoa");
+            //Id(x => x.Id, m => m.Generator(Generators.Guid));
+
+            //Property(x => x.Nome, m => m.Column("cNome");
+            //Property(x => x.DtNascimento);
+            //Property(x => x.Telefone);
+            //Property(x => x.Email);
+        }
     }
 }
